6c657cbba12c889db7d2ba3a097c28bf3ac5b453
Emin Tham <emintham@emintham>
2025-11-12 18:30:37 -0800

    fix

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #
 
-ln -s gitconfig ~/.gitconfig
-ln -s aliases ~/aliases
-ln -s ripgreprc ~/.ripgreprc
+# Get the absolute path of the directory where this script is located.
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-echo 'source $HOME/aliases' >> ~/.bashrc
-echo 'source $HOME/aliases' >> ~/.zshrc
+# Create symlinks using absolute paths.
+# The -f flag ensures that existing symlinks are overwritten.
+ln -sf "$SCRIPT_DIR/gitconfig" ~/.gitconfig
+ln -sf "$SCRIPT_DIR/aliases.sh" ~/aliases.sh
+ln -sf "$SCRIPT_DIR/ripgreprc" ~/.ripgreprc
+
+echo 'source $HOME/aliases.sh' >> ~/.bashrc
+echo 'source $HOME/aliases.sh' >> ~/.zshrc
